0a8985736e2cc5064b3f93b516f20bfbb2fc247d
DhruviKatrodiya <dhruviKatrodiya01@gmail.com>
2023-04-27 11:54:07 +0530

    Add new dtos

---
+++ Services/DTOs/ReportingPeriodFacilityElectricityGridMixDto.cs
@@ -0,0 +1,24 @@
+﻿namespace Services.DTOs
+{
+    public class ReportingPeriodFacilityElectricityGridMixDto
+    {
+        public int? Id { get; set; }
+        public int ReportingPeriodFacilityId { get; set; }
+        public int ElectricityGridMixComponentId { get; set; }
+        public string ElectricityGridMixComponentName { get; set; }
+        public int UnitOfMeasureId { get; set; }
+        public string UnitOfMeasureName { get; set; }
+        public decimal Content { get; set; }
+
+        public ReportingPeriodFacilityElectricityGridMixDto(int? id, int reportingPeriodFacilityId, int electricityGridMixComponentId, string electricityGridMixComponentName, int unitOfMeasureId, string unitOfMeasureName, decimal content)
+        {
+            Id = id;
+            ReportingPeriodFacilityId = reportingPeriodFacilityId;
+            ElectricityGridMixComponentId = electricityGridMixComponentId;
+            ElectricityGridMixComponentName = electricityGridMixComponentName;
+            UnitOfMeasureId = unitOfMeasureId;
+            UnitOfMeasureName = unitOfMeasureName;
+            Content = content;
+        }
+    }
+}
